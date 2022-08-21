e70dd8eab458ac245869d7b5a346ed289a9af7c8
YogzFX <110335811+YogzFX@users.noreply.github.com>
2022-08-21 08:29:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Other
-global.owner = ['6285815975368']
-global.premium = ['6285815975368']
-global.ownername = 'Liya'
-global.botname = 'AliyaBot'
-global.packname = '© by AliyaBot'
-global.gc = 'https://bit.ly/3KMlmv2'
-global.linkyt = 'https://youtube.com/channel/UC9Si3U0o9dGS9MDfJR5iF6Q'
-global.linkgc = 'https://chat.whatsapp.com/DwP6uHYBWBc6TBSsNJrzwN'
+global.owner = ['6289510598381']
+global.premium = ['6289510598381']
+global.ownername = 'YogzFX'
+global.botname = '𝒁𝒆𝒏𝒏𝑺𝒌𝒚'
+global.packname = 'Created By 𝒁𝒆𝒏𝒏𝑺𝒌𝒚'
+global.gc = 'https://chat.whatsapp.com/KNkpoT4oX5yFA18GlPttgl'
+global.linkyt = 'https://youtube.com/channel/UCAssIRzQX_Ohi1I8eWzcRxQ'
+global.linkgc = 'https://chat.whatsapp.com/KNkpoT4oX5yFA18GlPttgl'
 global.limitawal = '100'
-global.author = '@alya.xzy'
+global.author = '@YogzFX'
 global.sessionName = 'polo'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = 'terserah'
@@ -30,7 +30,7 @@ global.limitawal = {
     premium: "Infinity",
     free: 100
 }
-global.gambar = 'https://telegra.ph/file/7858fc96fffda74b3ab44.jpg' //disini gambar bot nya lu bisa ganti pink gambar ini dengan link gambar punya lu
+global.gambar = 'https://telegra.ph/file/981eb83e7129c12e24d20.jpg' //disini gambar bot nya lu bisa ganti pink gambar ini dengan link gambar punya lu
 global.sc = fs.readFileSync('./media/sc.jpg')
 global.tq = fs.readFileSync('./media/tq.jpg')
 global.menu = fs.readFileSync('./media/menu.jpg')
